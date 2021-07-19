554dc6702c4f9c91bf885063b9c285ec1074802e
Yulin Dong <yulin.dong@avalabs.org>
2021-07-18 18:25:21 -0600

    fix: formatting

--- src/keyChain.ts
+++ src/keyChain.ts
@@ -1,22 +1,21 @@
-import { 
+import {
     Avalanche
-  } from "avalanche"
-  import { 
-    AVMAPI, 
-    KeyChain 
-  } from "avalanche/dist/apis/avm"
-    
-  const ip: string = 'localhost'
-  const port: number = 9650
-  const protocol: string = 'http'
-  const networkID: number = 12345
-  const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
-  const xchain: AVMAPI = avalanche.XChain()
-    
-  const main = async (): Promise<any> => {
+} from "avalanche"
+import {
+    AVMAPI,
+    KeyChain
+} from "avalanche/dist/apis/avm"
+
+const ip: string = 'localhost'
+const port: number = 9650
+const protocol: string = 'http'
+const networkID: number = 12345
+const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
+const xchain: AVMAPI = avalanche.XChain()
+
+const main = async (): Promise<any> => {
     const keyChain: KeyChain = xchain.keyChain()
     console.log(keyChain)
-  }
-      
-  main()
-    
+}
+
+main()
--- src/peers.ts
+++ src/peers.ts
@@ -1,19 +1,19 @@
-import { 
+import {
     Avalanche,
-  } from "avalanche"
-  import { InfoAPI } from "avalanche/dist/apis/info"
-  import { PeersResponse } from "avalanche/dist/common/interfaces"
-    
-  const ip: string = 'localhost'
-  const port: number = 9650
-  const protocol: string = 'http'
-  const networkID: number = 12345
-  const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
-  const info: InfoAPI = avalanche.Info()
-    
-  const main = async (): Promise<any> => {
+} from "avalanche"
+import { InfoAPI } from "avalanche/dist/apis/info"
+import { PeersResponse } from "avalanche/dist/common"
+
+const ip: string = 'localhost'
+const port: number = 9650
+const protocol: string = 'http'
+const networkID: number = 12345
+const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
+const info: InfoAPI = avalanche.Info()
+
+const main = async (): Promise<any> => {
     const peers: PeersResponse[] = await info.peers([])
     console.log(peers)
-  }
-      
-  main()
+}
+
+main()
